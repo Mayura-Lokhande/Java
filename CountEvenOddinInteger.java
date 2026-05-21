20f25f85a2a1b6587004a7ff3c47e0a03e4b0cf6
Mayura-Lokhande <mayura.lokhande@techolution.com>
2026-05-21 16:59:14 +0530

    Refactor CountEvenOddinInteger class and enhance input
    Refactored class name and improved user prompts and comments.

--- CountEvenOddinInteger.java
+++ CountEvenOddinInteger.java
@@ -1,32 +1,50 @@
 package practise;
-import java.util.*;
-public class CountEvenOddinInteger {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		
-		int n = sc.nextInt();
-		String temp = Integer.toString(n);
-		int arr[] = new int[temp.length()];
-		int countEven = 0;
-		int countOdd = 0;
-		for(int i=0; i<temp.length(); i++) {
-			arr[i]=temp.charAt(i)-'0';
-		}
-		for(int i=0; i<temp.length(); i++) {
-			if(arr[i]%2==0) {
-				countEven++;
-			}
-			else if(arr[i]%2==1){
-				countOdd++;
-			}
-		}
-		System.out.println("Even --> " + countEven);
 
-		System.out.println("Odd --> " + countOdd);
+import java.util.Scanner;
 
-		
-		sc.close();
-		
-	}
+public class CountEvenOddInInteger {
+
+    public static void main(String[] args) {
+
+        // Create Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Accept integer input from user
+        System.out.print("Enter an integer value: ");
+        int number = scanner.nextInt();
+
+        /*
+         * Convert integer into string format
+         * to access each digit individually
+         */
+        String numericString = Integer.toString(number);
+
+        // Counters for even and odd digits
+        int evenDigitCount = 0;
+        int oddDigitCount = 0;
+
+        /*
+         * Traverse each digit of the number
+         * and classify as even or odd
+         */
+        for (int index = 0; index < numericString.length(); index++) {
+
+            // Convert character digit into integer value
+            int digit = numericString.charAt(index) - '0';
+
+            // Check parity of digit
+            if (digit % 2 == 0) {
+                evenDigitCount++;
+            } else {
+                oddDigitCount++;
+            }
+        }
+
+        // Display final output
+        System.out.println("Total Even Digits --> " + evenDigitCount);
+        System.out.println("Total Odd Digits  --> " + oddDigitCount);
+
+        // Close scanner resource
+        scanner.close();
+    }
 }
