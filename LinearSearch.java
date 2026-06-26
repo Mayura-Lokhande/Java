e119aef336ca3a70c5ed485b7e947ea52c8c5fe8
Mayura-Lokhande <mayura.lokhande@techolution.com>
2026-06-26 15:17:52 +0530

    Refactor LinearSearch to include user input
    Updated the linear search implementation to allow user input for the value to search and display the array elements.

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,23 +1,34 @@
 package searching_algo;
-import java.util.*;
-public class LinearSearch {
-	public static void main(String args[]){
-		  Scanner sc = new Scanner(System.in);
-	      int array[] = {10, 40, 50, 87, 45, 67, 45};
-	      int size = array.length;
-	      int value = 45;
-	      int num = -1;
 
-	      for (int i=0 ;i< size; i++){
-	         if(array[i]==value){
-	            num = i;
-	            break;
-	         }
-	      }
-	      if(num>=0) {
-	    	  System.out.println("FOUND ON INDEX " + num);
-	      }else {
-	    	  System.out.println("NOT FOUND");
-	      }
-	 }
+import java.util.*;
+
+public class LinearSearch {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int array[] = {10, 40, 50, 87, 45, 67, 45};
+
+        System.out.println("Array elements: " + Arrays.toString(array));
+
+        System.out.print("Enter value to search: ");
+        int value = sc.nextInt();
+
+        int index = -1;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index != -1) {
+            System.out.println("FOUND ON INDEX: " + index);
+        } else {
+            System.out.println("NOT FOUND");
+        }
+
+        sc.close();
+    }
 }
