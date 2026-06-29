33e3ad6857b902571c7ba97c5d6dbc3e09168b52
Mayura-Lokhande <mayura.lokhande@techolution.com>
2026-06-29 15:02:59 +0530

    Add user input for two numbers in Java program

--- AddTwoNumbersByUserInput.java
+++ AddTwoNumbersByUserInput.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class Main{
+    //check 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int firstNumber = sc.nextInt(); 
