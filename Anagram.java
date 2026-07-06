c905b555e3153850c652d7219ba8497c88d1d10a
Mayura-Lokhande <mayura.lokhande@techolution.com>
2026-07-06 16:39:19 +0530

    Correct typo in comment in Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,7 +1,7 @@
 package string;
 import java.util.Scanner;
 public class Anagram {
-
+// this is a programming languuage 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
