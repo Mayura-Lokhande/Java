2789435d0445ef1280efba6977af477dd3f2c8ca
Mayura-Lokhande <mayura.lokhande@techolution.com>
2026-07-31 18:48:25 +0530

    Refactor variable names for user input addition

--- AddTwoNumbersByUserInput.java
+++ AddTwoNumbersByUserInput.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int firstNumber = sc.nextInt(); 
-        int secondNumber = sc.nextInt();
-        int result = firstNumber + secondNumber;
+        int a = sc.nextInt(); 
+        int b = sc.nextInt();
+        int result = a + b;
                 System.out.print("result: " + result);
+        
     }
 }
